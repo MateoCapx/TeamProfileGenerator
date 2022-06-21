58775f056f776c98117ddb7bd17a0d16729d93fd
Mateo <mateom.realtor@gmail.com>
2022-06-21 15:38:11 -0400

    updated Index.js

--- index.js
+++ index.js
@@ -6,250 +6,169 @@ const generatePage = require('./src/generatePage');
 //lib modules
 const Engineer = require('./lib/Engineer')
 const Intern = require('./lib/Intern')
-const Manager = require('./lib/Manager')
+const Manager = require('./lib/Manager');
 
 
-const arrayData =[];
+
+const arrayData = [];
 
 // return inquirer
-const questions = async() => {
- const answers = await inquirer
-   .prompt([
-    {
-      type: 'input',
-      name: ' Name',
-      message: 'Enter Team Manager Name (Required)',
-      validate: enterName => {
-        if (enterName) {
-          return true;
-        } else {
-          console.log('You need to enter a Team member name!');
-          return false;
-        }
-      }
-    },
-
-
-    {
-      type: 'input',
-      name: 'employeeId',
-      message: 'Enter Employee ID (Required)',
-      validate: employeeInput => {
-        const pass = employeeInput.match(/^[1-9]\d*$/) //<-- Validate a number
-        if (pass) {
-            return true;
-          }
-          return "Please enter a valid employee ID";
-      }
-    },
-
-    
-    {
-      type: 'input',
-      name: 'emailAddress',
-      message: 'Enter Email Address (Required)',
-      validate: emailInput => {
-        const pass = emailInput.match(/\S+@\S+\.\S+/) //<-- Validate a email address
-        if (pass) {
-            return true;
-          }
-          return "Please enter a valid email address";
-      }
-    },
-
-
-    {
-      type: 'input',
-      name: 'officeNumber',
-      message: 'Enter Office number!',
-      validate: officeNumberInput => {
-        const pass = officeNumberInput.match(/^[1-9]\d*$/) //<-- Validate a number
-        if (pass) {
-            return true;
-          }
-          return "Please enter a valid office number";
-      }
-      
-    },
-
-    {
-        type: 'list',
-        name: 'role',
-        message: 'Who Would you like on Your Team?',
-        choices: ['Engineer','Intern', 'Finish Building My Team'],
-      }
-       
-   ])
-
-
-
-
-// Addition questions for Engineer  
-   if(answers.role === 'Engineer'){
-    const engineerQuestions = await inquirer
-   .prompt([
-        {
-          type: 'input',
-          name: ' Name',
-          message: 'Enter Engineer Name (Required)',
-          validate: enterName => {
-            if (enterName) {
-              return true;
-            } else {
-              console.log('You need to enter a Team member name!');
-              return false;
-            }
-          }
-        },
-
-        {
-            type: 'input',
-            name: 'employeeId',
-            message: 'Enter Engineer Employee ID (Required)',
-            validate: employeeInput => {
-              const pass = employeeInput.match(/^[1-9]\d*$/) //<-- Validate a number
-              if (pass) {
-                  return true;
+const questions = async () => {
+    const answers = await inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: ' Name',
+                message: 'Enter Your Name (Required)',
+                validate: enterName => {
+                    if (enterName) {
+                        return true;
+                    } else {
+                        console.log('You need to enter a Team member name!');
+                        return false;
+                    }
                 }
-                return "Please enter a valid employee ID";
-            }
-          },
-      
-          
-          {
-            type: 'input',
-            name: 'emailAddress',
-            message: 'Enter Engineer Email Address (Required)',
-            validate: emailInput => {
-              const pass = emailInput.match(/\S+@\S+\.\S+/) //<-- Validate a email address
-              if (pass) {
-                  return true;
+            },
+
+
+            {
+                type: 'input',
+                name: 'employeeId',
+                message: 'Enter Employee ID (Required)',
+                validate: employeeInput => {
+                    const pass = employeeInput.match(/^[1-9]\d*$/) //<-- Validate a number
+                    if (pass) {
+                        return true;
+                    }
+                    return "Please enter a valid employee ID";
                 }
-                return "Please enter a valid email address";
-            }
-          },
-      
+            },
 
-    {
-        type: 'input',
-        name: 'gitHubUsername',
-        message: 'Enter Engineer GitHub Username',
-        validate: gitHubUsername => {
-          const pass = gitHubUsername.match('^(https?:\\/\\/)?'+ // protocol
-          '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
-          '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-          '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-          '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-          '(\\#[-a-z\\d_]*)?$','i'); //) //<-- Validate a number
-          if (pass) {
-              return true;
-            }
-            return "Please enter a valid office number";
-        }
-        
-      },
 
-      
-    ])
-   arrayData.push(engineerQuestions),
-      console.log(arrayData)
-    
+            {
+                type: 'input',
+                name: 'emailAddress',
+                message: 'Enter Email Address (Required)',
+                validate: emailInput => {
+                    const pass = emailInput.match(/\S+@\S+\.\S+/) //<-- Validate a email address
+                    if (pass) {
+                        return true;
+                    }
+                    return "Please enter a valid email address";
+                }
+            },
+
+            {
+                type: 'list',
+                name: 'role',
+                message: 'What is Your Role?',
+                choices: ['Engineer', 'Intern', , 'Manager', 'Finish Building My Team'],
+            }
+        ])
+
+
+    // Addition questions for Engineer  
+    if (answers.role === 'Engineer') {
+        const engineerQuestions = await inquirer
+            .prompt([
+
+                {
+                    type: 'input',
+                    name: 'gitHubUsername',
+                    message: 'Enter Engineer GitHub Username',
+                    validate: gitHubUsername => {
+                        // validaing to make sure user inputs a correct URL 
+                        const pass = gitHubUsername.match('^(https?:\\/\\/)?' + // protocol
+                            '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+                            '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+                            '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+                            '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+                            '(\\#[-a-z\\d_]*)?$', 'i'); //) //<-- Validate a number
+                        if (pass) {
+                            return true;
+                        }
+                        return "Please enter a valid office number";
+                    }
+                },
+            ])
+        arrayData.push(answers, engineerQuestions),
+            console.log(arrayData)
+
     }
 
 
+    if (answers.role === 'Manager') {
+        const managerQuestions = await inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    name: 'officeNumber',
+                    message: 'Enter Office number!',
+                    validate: officeNumberInput => {
+                        const pass = officeNumberInput.match(/^[1-9]\d*$/) //<-- Validate a number
+                        if (pass) {
+                            return true;
+                        }
+                        return "Please enter a valid office number";
+                    }
+                },
+            ])
 
-
-
-    // Addition questions for intern
-    
-     if(answers.role === 'Intern'){
-    const internQuestions = await inquirer
-   .prompt([
-        {
-          type: 'input',
-          name: ' Name',
-          message: 'Enter Intern Name (Required)',
-          validate: enterName => {
-            if (enterName) {
-              return true;
-            } else {
-              console.log('You need to enter a Team member name!');
-              return false;
-            }
-          }
-        },
-
-        {
-            type: 'input',
-            name: 'employeeId',
-            message: 'Enter Intern Employee ID (Required)',
-            validate: employeeInput => {
-              const pass = employeeInput.match(/^[1-9]\d*$/) //<-- Validate a number
-              if (pass) {
-                  return true;
-                }
-                return "Please enter a valid employee ID";
-            }
-          },
-      
-          
-          {
-            type: 'input',
-            name: 'emailAddress',
-            message: 'Enter Intern Email Address (Required)',
-            validate: emailInput => {
-              const pass = emailInput.match(/\S+@\S+\.\S+/) //<-- Validate a email address
-              if (pass) {
-                  return true;
-                }
-                return "Please enter a valid email address";
-            }
-          },
-      
-
-    {
-        type: 'input',
-        name: 'internSchool',
-        message: 'Enter Intern School', 
-      },
-
-      
-    ])
-   arrayData.push(internQuestions),
-      console.log(arrayData)
-    
-    }
+        arrayData.push(answers, managerQuestions),
+            console.log(arrayData)
 
     }
-    
 
 
+    if (answers.role === 'Intern') {
+        const internQuestions = await inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    name: 'School',
+                    message: 'Enter Intern School',
+                },
+            ])
+
+        arrayData.push(answers, internQuestions),
+            console.log(arrayData)
+
+    }
+}
 
 questions();
 
 
-//  module.exports = prompt
+async function promptQuestions() {
+    await questions()
+
+    const addMemeberAnswer = await inquirer
+        .prompt([
+            {
+                name: 'addMember',
+                type: 'list',
+                choices: ['Add a Member', 'Create Team'],
+                message: " What would you like to do next? "
+            }
+        ])
+    if (addMemeberAnswer.addMember === 'Add a Member') {
+        return  questions()
+    }
+    return createTeam();
+}
+
+promptQuestions();
 
 
+function createTeam(){
+    console.log(" Heyyyyyyyyyyy !!!!!!!!!!!!!!!!!")
+//   fs.writeFile('./dist/index.html', generatePage(arryData), err => {
+//             if (err) {
+//                 console.log(err)
+//             } else {
+//                 console.log(" Page created! Check out index.html in this directory to see it! ")
+//             }
+//         })
 
-
-
-
-
-
-
-
-   // .then(function(data){
-//     console.log(`Heyy ${JSON.stringify(data)}`)
-
-    
-//     fs.writeFile('./dist/index.html', generateSite(data), err=>{
-//       if(err){
-//         console.log(err)
-//       }else{
-//         console.log(" Page created! Check out index.html in this directory to see it! ")
-//       }
-//     })
-//   }
-
+}
 
